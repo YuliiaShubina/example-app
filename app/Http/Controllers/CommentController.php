3c5199de4199f1fea5cabd4153d2465ad08a02dd
YuliiaShubina <145291057+YuliiaShubina@users.noreply.github.com>
2025-12-11 01:16:59 +0000

    Removed notifications feature

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Comment;
 use App\Models\Habit;
-use App\Notifications\PostInteracted;
 
 class CommentController extends Controller
 {
@@ -21,20 +20,6 @@ class CommentController extends Controller
         $comment->user_id = auth()->id();
         $comment->habit_id = $habit->id;
         $comment->save();
-
-        if ($habit->user_id !== auth()->id()) {
-    $habit->user->notify(
-        new PostInteracted(
-            auth()->user(),  
-            $habit,           
-            'comment',        
-            [           
-                'comment_id'   => $comment->id,
-                'comment_body' => $comment->body,
-            ]
-        )
-    );
-}
 
         if ($request->wantsJson()) {
         $comment->load('user');
