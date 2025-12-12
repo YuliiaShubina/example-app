54cdd19ae9c657eba8a348bf5f3f3c98a6dd42a7
YuliiaShubina <145291057+YuliiaShubina@users.noreply.github.com>
2025-12-12 04:58:25 +0000

    Update Habit.php

--- app/Models/Habit.php
+++ app/Models/Habit.php
@@ -13,11 +13,6 @@ class Habit extends Model {
     public function user() {
         return $this->belongsTo(User::class);
     }
-
-     public function comment() {
-        return $this->hasMany(Comment::class);
-    }
-
     public function likes() {
         return $this->morphMany(Like::class, 'likeable');
     }
